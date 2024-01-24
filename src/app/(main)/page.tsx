0e9465e3e7e855446c5173ed1b2c55d4fb00c5a1
liuyongqi <245938767@qq.com>
2024-01-24 15:42:20 +0800

    feat:add my self career

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -5,7 +5,7 @@ import { Container } from '../../components/ui/Container';
 import { BlogPosts } from './blog/BlogPosts';
 import { Headline } from './Headline';
 import { Photos } from './Photos';
-
+import { Resume } from './Resume';
 
 export default function BlogHomePage() {
   return (
@@ -18,14 +18,14 @@ export default function BlogHomePage() {
         <div className="mx-auto grid max-w-xl grid-cols-1 gap-y-20 lg:max-w-none lg:grid-cols-2">
           <div className="flex flex-col gap-6 pt-6">
             <h2 className="flex items-center text-sm font-semibold text-zinc-900 dark:text-zinc-100">
-              <PencilSwooshIcon className="h-5 w-5 flex-none" />
+              <PencilSwooshIcon className="size-5 flex-none" />
               <span className="ml-2">近期文章</span>
             </h2>
             <BlogPosts />
           </div>
           <aside className="space-y-10 lg:sticky lg:top-8 lg:h-fit lg:pl-16 xl:pl-20">
-            {/* <Newsletter />
-            <Resume /> */}
+            {/* <Newsletter /> */}
+            <Resume />
           </aside>
         </div>
       </Container>
